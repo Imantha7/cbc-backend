b297d9cda4b878318851b5f725fe63a827c9b21e
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-19 19:36:33 +0530

    sorted order by orderId 👌😊

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,132 +2,141 @@ import Order from "../models/order.js";
 import Product from "../models/product.js";
 import { isCustomer } from "./userController.js";
 
-export async function createOrder(req,res){
-  
-  if(!isCustomer){
+export async function createOrder(req, res) {
+  if (!isCustomer) {
     res.json({
-      message: "Please login as customer to create orders"
-    })
+      message: "Please login as customer to create orders",
+    });
   }
 
-  try{
-    const latestOrder = await Order.find().sort({date : -1}).limit(1)
+  try {
+    const latestOrder = await Order.find().sort({ orderId: -1 }).limit(1);
+    console.log(latestOrder);
 
-    let orderId
+    let orderId;
 
-    if(latestOrder.length == 0){
-      orderId = "CBC0001"
-    }else{
-      const currentOrderId = latestOrder[0].orderId
+    if (latestOrder.length == 0) {
+      orderId = "CBC0001";
+    } else {
+      const currentOrderId = latestOrder[0].orderId;
 
-      const numberString =  currentOrderId.replace("CBC","")
+      const numberString = currentOrderId.replace("CBC", "");
 
-      const number = parseInt(numberString)
+      const number = parseInt(numberString);
 
       const newNumber = (number + 1).toString().padStart(4, "0");
 
-      orderId = "CBC" + newNumber
+      orderId = "CBC" + newNumber;
     }
 
-    const newOrderData = req.body
+    const newOrderData = req.body;
 
-    const newProductArray = []
+    const newProductArray = [];
 
-    for(let i=0;i<newOrderData.orderedItems.length;i++){
-
+    for (let i = 0; i < newOrderData.orderedItems.length; i++) {
       const product = await Product.findOne({
-        productId : newOrderData.orderedItems[i].productId
-      })
+        productId: newOrderData.orderedItems[i].productId,
+      });
 
-      if(product == null){
+      if (product == null) {
         res.json({
-          message: "Product with id"+newOrderData.orderedItems[i].productId+" not found"
-        })
-        return
+          message:
+            "Product with id " +
+            newOrderData.orderedItems[i].productId +
+            " not found",
+        });
+        return;
       }
 
       newProductArray[i] = {
-        name : product.productName,
-        price : product.price,
-        quantity : newOrderData.orderedItems[i].qty,
-        image : product.images[0]
-      }
-
+        name: product.productName,
+        price: product.lastPrice,
+        quantity: newOrderData.orderedItems[i].qty,
+        image: product.images[0],
+      };
     }
-    console.log(newProductArray)
+    console.log(newProductArray);
 
-    newOrderData.orderedItems = newProductArray
+    newOrderData.orderedItems = newProductArray;
 
+    newOrderData.orderId = orderId;
+    newOrderData.email = req.user.email;
 
-    newOrderData.orderId = orderId
-    newOrderData.email = req.user.email
+    const order = new Order(newOrderData);
 
-    const order = new Order(newOrderData)
-
-    const saveOrder = await order.save()
+    const savedOrder = await order.save();
 
     res.json({
-      message: "Order created"
-      order: saveOrder
-    })
-
-
-
-  }catch(error){
+      message: "Order created",
+      order : savedOrder
+    });
+  } catch (error) {
     res.status(500).json({
-      message: error.message
-    })
-  }
-
-}
-
-export async function getOrders(req,res){
-  try{
-    const orders = await Order.find({email : req.user.email})
-
-    res.json(orders)
-
-  }catch(error){
-    res.status(500).json({
-      message: error.message
-    })
+      message: error.message,
+    });
   }
 }
 
-export async function getQuote(req,res){
+export async function getOrders(req, res) {
+  
+    
+  try {
+    if (isCustomer(req)) {
+    const orders = await Order.find({ email: req.user.email });
 
-  try{
+    res.json(orders);
+    return;
+    }else if(isAdmin(req)){
+      const orders = await Order.find({});
 
-    const newOrderData = req.body
+      res.json(orders);
+      return;
+    }else{
+      res.json({
+        message: "Please login to view orders"
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+}
 
-    const newProductArray = []
+export async function getQuote(req, res) {
+  
+  try {
+    const newOrderData = req.body;
+
+    const newProductArray = [];
 
     let total = 0;
     let labeledTotal = 0;
+    console.log(req.body)
 
-    for(let i=0;i<newOrderData.orderedItems.length;i++){
-
+    for (let i = 0; i < newOrderData.orderedItems.length; i++) {
       const product = await Product.findOne({
-        productId : newOrderData.orderedItems[i].productId
-      })
+        productId: newOrderData.orderedItems[i].productId,
+      });
 
-      if(product == null){
+      if (product == null) {
         res.json({
-          message: "Product with id"+newOrderData.orderedItems[i].productId+" not found"
-        })
-        return
+          message:
+            "Product with id " +
+            newOrderData.orderedItems[i].productId +
+            " not found",
+        });
+        return;
       }
       labeledTotal += product.price * newOrderData.orderedItems[i].qty;
-      total += product.lastPrice * newOrderData.orderedItems[i].qty; 
-
+      total += product.lastPrice * newOrderData.orderedItems[i].qty;
       newProductArray[i] = {
-        name : product.productName,
-        price : product.lastPrice,
+        name: product.productName,
+        price: product.lastPrice,
         labeledPrice: product.price,
-        quantity : newOrderData.orderedItems[i].qty,
-        image : product.images[0]
-      }
-
+        quantity: newOrderData.orderedItems[i].qty,
+        image: product.images[0],
+      };
     }
     console.log(newProductArray);
     newOrderData.orderedItems = newProductArray;
@@ -137,11 +146,12 @@ export async function getQuote(req,res){
       orderedItems: newProductArray,
       total: total,
       labeledTotal: labeledTotal,
-    })
+    });
 
-  }catch(error){
+
+  } catch (error) {
     res.status(500).json({
-      message: error.message
-    })
+      message: error.message,
+    });
   }
 }
