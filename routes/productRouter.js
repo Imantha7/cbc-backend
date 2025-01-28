bc90797cdbcb515f0510deaa3c34007fa841014d
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-28 20:36:07 +0530

    created products search function ✅

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/productController.js';
+import { createProduct, deleteProduct, getProductById, getProducts, searchProducts, updateProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
+productRouter.get("/search/:query",searchProducts)
 productRouter.get("/:productId",getProductById)
 productRouter.delete("/:productId",deleteProduct)
 productRouter.put("/:productId",updateProduct)
 
-export default productRouter;  
+
+export default productRouter; 
